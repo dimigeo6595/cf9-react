eb35f758912beffd86b387e2ccd47c416fa2f737
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-05-13 19:57:27 +0300

    feat: add Header, Footer and Layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,74 @@
+// import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
+// import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
+// import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
 
+import Layout from "./components/Layout.tsx";
+import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFuntionalComponentWithProps.tsx";
+
+// type PropsA = {
+//   title: string,
+//   description: string,
+// }
+// type PropsB = {
+//   title: string,
+//   description: string,
+// }
+//
+// // UNION
+// type Status = "Error" | "Info" | "Warning";
+//
+// // Tuples
+// type Coordinates = [number, number];
+//
+// //Functions
+// type ClickHandler = (event: MouseEvent) => void;
+//
+// //INTERSECTION
+// type Props = PropsA & PropsB;
+//
+// interface PropsTwo {
+//   title: string,
+//   description: string,
+// }
+//
+// interface PropsTwo {
+//   price: number,
+//   sort: number,
+// }
+// // PropsTwo = {title, description, price, sort}
+//
+// interface PropsThree extends PropsTwo {
+//   image: string,
+// }
+// // PropsThree = {title, description, price, sort, image}
 
 function App() {
 
-
   return (
-    <>
+      <>
+        {/*LESSON TWO*/}
+        {/*<FunctionalComponent />*/}
+        {/*<FunctionalComponent />*/}
+        {/*<FunctionalComponent />*/}
+        {/*<FunctionalComponent />*/}
+        {/*<ClassComponent />*/}
+        {/*<ArrowFunctionalComponent />*/}
 
-    </>
+        {/*LESSON THREE*/}
+        {/*<ArrowFunctionalComponentWithProps title="Heading 1"/>*/}
+        {/*<ArrowFunctionalComponentWithProps*/}
+        {/*    title="Second Title"*/}
+        {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet, deserunt dicta excepturi fugit ipsa modi perferendis quo repellat voluptates."*/}
+        {/*/>*/}
+
+        <Layout>
+          <ArrowFunctionalComponentWithProps
+              title="Second Title"
+              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet, deserunt dicta excepturi fugit ipsa modi perferendis quo repellat voluptates."
+          />
+        </Layout>
+
+      </>
   )
 }
 
