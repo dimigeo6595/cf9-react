590e624fa257dcf6817a00277c690cb2fadfc8c6
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-05-13 21:07:22 +0300

    feat(ui): implement ui on layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
 const Footer = () => {
+    const currentYear: number = new Date().getFullYear();
+
+
     return (
         <>
-            <footer>
-                This is a Footer area.
+            <footer className="bg-cf-dark-grey text-white">
+               <div className = "container mx-auto py-8 text-center">
+                   &copy; {currentYear} Coding Factory 9. All Rights Reserved.
+               </div>
             </footer>
         </>
     )
