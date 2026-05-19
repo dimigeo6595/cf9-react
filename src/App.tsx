639b22b01778091b69685b48ee55511a95d1cc94
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-05-19 20:54:41 +0300

    feat: add FocusInput component

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@
 // import NameChangerWithTitle from "./components/LessonFive/NameChangerWithTitle.tsx";
 // import AutoRedirect from "./components/LessonSix/AutoRedirect.tsx";
 import Layout from "./components/Layout.tsx";
-import WindowSize from "./components/LessonSix/WindowSize.tsx";
+// import WindowSize from "./components/LessonSix/WindowSize.tsx";
+import FocusInput from "./components/LessonSix/FocusInput.tsx";
 // import AutoRedirectAdvanced from "./components/LessonSix/AutoRedirectAdvanced.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
@@ -98,7 +99,8 @@ function App() {
             <Layout addClasses="bg-gray-50">
                 {/*<AutoRedirect/>*/}
                 {/*<AutoRedirectAdvanced/>*/}
-                <WindowSize/>
+                {/*<WindowSize/>*/}
+                <FocusInput/>
             </Layout>
         </>
     )
