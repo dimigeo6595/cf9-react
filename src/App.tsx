c683165cc1e8b65f5b33b63ee378f621faaa6740
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-05-20 18:10:42 +0300

    feat: add PeviousValue component

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@
 // import AutoRedirect from "./components/LessonSix/AutoRedirect.tsx";
 import Layout from "./components/Layout.tsx";
 // import WindowSize from "./components/LessonSix/WindowSize.tsx";
-import FocusInput from "./components/LessonSix/FocusInput.tsx";
+// import FocusInput from "./components/LessonSix/FocusInput.tsx";
+import PreviousValue from "./components/LessonSix/PreviousValue.tsx";
 // import AutoRedirectAdvanced from "./components/LessonSix/AutoRedirectAdvanced.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
@@ -100,7 +101,8 @@ function App() {
                 {/*<AutoRedirect/>*/}
                 {/*<AutoRedirectAdvanced/>*/}
                 {/*<WindowSize/>*/}
-                <FocusInput/>
+                {/*<FocusInput/>*/}
+                <PreviousValue/>
             </Layout>
         </>
     )
